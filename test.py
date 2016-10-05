7f38ad4936d4164fd2e9fcb3e34f8939d1b2ae2d
Abhinav Anand <erabhinav@outlook.com>
2016-10-05 22:26:10 +0530

    further testing added

--- test.py
+++ test.py
@@ -1,25 +1,38 @@
 # test script for WalMart.py module
 
-from WalMart import Session
+from WalMart import Session, ItemProcess
 import atexit
 base_url = 'http://grocery.walmart.com/'
 signout_url = 'usd-estore/register/signoutcontainer.jsp'
 signin_url = 'usd-estore/m/register/signin-only.jsp'
 home_url = 'usd-estore/m/home.jsp'
-
+product_url = 'http://grocery.walmart.com/usd-estore/m/product-detail.jsp?skuId='
 # initialize the session
 walmart = Session(base_url, signin_url, home_url, signout_url)
 
 
 # use this method when you are accessing the web page for the first time
+# now while logging in, it will itself display, which user has logged in and
+# also if login fails for any reason, the script will try three more times before quitting
+# and it will also make sure the correct user has logged in
 walmart.login('jeffreylunt@gmail.com', 'temp_dev')
+
 # uncomment the below line and comment the above line if you are coming back to this website again
 # now you can enter the particular session to resume
-#walmart.load_prev_session()
-#walmart.load_prev_session('cookies.pkl')
+# walmart.load_prev_session()
+# walmart.load_prev_session('cookies.pkl')
 
+# below is the method to add items to cart
+# you have to provide data in list of sku id
+# [[skuid, quantity], [skuid, quantity]]
+process_items = ItemProcess()
+
+# by using below method you can fetch the contents of cart and verify
+process_items.add_item_to_cart([[3000038027, 2], [1025774, 2]])
+
+#
 # for searching any item
-#walmart.search('banana')
+# walmart.search('banana')
 
 # for saving the current state of the session
 # this will save the cookie in the default file, thus overwriting the previous one
@@ -43,7 +56,7 @@ walmart.take_screenshot()
 # sign out of your current session
 # walmart.sign_out()
 
-# close the session
+# close the session and browser
 walmart.close()
 
 
